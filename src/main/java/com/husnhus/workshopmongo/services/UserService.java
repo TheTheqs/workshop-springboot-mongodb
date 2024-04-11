84ed48ffbe9d13ce571210cb22cfb2c6dc5db838
Matheqs <matheqs@outlook.com>
2024-04-11 10:14:16 -0300

    FindById USER

--- src/main/java/com/husnhus/workshopmongo/services/UserService.java
+++ src/main/java/com/husnhus/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.husnhus.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.husnhus.workshopmongo.domain.User;
 import com.husnhus.workshopmongo.repository.UserRepository;
+import com.husnhus.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
