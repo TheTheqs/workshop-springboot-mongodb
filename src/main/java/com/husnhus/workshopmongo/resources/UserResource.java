bc7e4cedbba3261825f2e27dc24368ed9b408638
Matheqs <matheqs@outlook.com>
2024-04-11 09:46:46 -0300

    DTO parameters to get users

--- src/main/java/com/husnhus/workshopmongo/resources/UserResource.java
+++ src/main/java/com/husnhus/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.husnhus.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.husnhus.workshopmongo.domain.User;
+import com.husnhus.workshopmongo.dto.UserDTO;
 import com.husnhus.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findaAll(){
+	public ResponseEntity<List<UserDTO>> findaAll(){
 				
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toUnmodifiableList()); 
+		return ResponseEntity.ok().body(listDto);
 	}
 }
